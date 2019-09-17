4b42d0e60f720b8abf13418cf068f388380e1fdc
vagrant <Kennethzhang374@gmail.com>
2019-09-17 06:11:05 +0000

    group by exercise done

---
+++ 3_queries_group-by/4_totalSubmission.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name, COUNT(assignment_id) as total_submissions
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = student_id
+GROUP BY cohorts.name
+ORDER BY COUNT(assignment_id);
