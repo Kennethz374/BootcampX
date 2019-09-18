5d892dc77c169f95b0ceb17b18021ec0488aeefb
Kenneth <sally@sallyde-MacBook-Air.local>
2019-09-18 14:53:34 -0700

    connected database and js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const process = require('process');
+let cohortName = process.argv[2]
+pool.query(`
+SELECT 
+DISTINCT teachers.name AS teacher, 
+cohorts.name AS cohort
+
+FROM teachers 
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  // console.log(res.rows);
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}:  ${user.teacher}`);
+  })
+});
